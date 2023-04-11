12bfbe064205cb59b132910e69f1ee2a288d5a2d
Youssef-zr <yn-neinaa@hotmail.com>
2023-04-11 15:29:21 +0000

    fixed categry margin

--- static-assets/lw-jquery-whatsapp-insta-order/js/lw-jquery-whatsapp-insta-order.js
+++ static-assets/lw-jquery-whatsapp-insta-order/js/lw-jquery-whatsapp-insta-order.js
@@ -819,7 +819,7 @@
               .addClass("hidden"),
               (m.enableOrderBtn = !1);
             t =
-              "https://api.whatsapp.com/send?phone=" +
+              "https://wa.me/send?phone=" +
               c.checkoutMethods.orderByWhatsApp.mobileNumber +
               "&text=";
             return (
